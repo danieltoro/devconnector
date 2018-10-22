50288b480f1a094e82f3e3a82ae44e3b436a1b27
danieltoro <d.toro@outlook.com>
2018-10-21 22:15:27 -0300

    added a private route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import store from './store';
 
@@ -12,9 +19,33 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import Dashboard from './components/dashboard/Dashboard';
+
 import './App.css';
 
+// Check for token
+if(localStorage.jwtToken) {
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken);
 
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(localStorage.jwtToken);
+
+    //Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if(decoded.exp < currentTime) {
+        // Logout User
+        store.dispatch(logoutUser());
+        // Clear current Profile
+        store.dispatch(clearCurrentProfile());
+
+        // Redirect to login
+        window.location.href = '/login';
+    }
+}
 
 class App extends Component {
     render() {
@@ -27,6 +58,9 @@ class App extends Component {
                         <div className="container">
                             <Route exact path="/register" component={ Register }/>
                             <Route exact path="/login" component={ Login }/>
+                            <Switch>
+                                <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
+                            </Switch>
                         </div>
                         <Footer/>
                     </div>
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,16 +1,67 @@
 import axios from 'axios';
+import jwt_decode from  'jwt-decode';
 
-import { GET_ERRORS } from '../actions/types';
+import { GET_ERRORS, SET_CURRENT_USER } from '../actions/types';
+
+import setAuthToken from '../utils/setAuthToken';
 
 // Register User
-export const registerUser = userData => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
     axios
         .post('api/users/register', userData)
-        .then(res => console.log(res.data))
+        .then(res => history.push('/login'))
         .catch(err =>
             dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
             })
         );
+};
+
+// Login User - Get User Token
+export const loginUser = (userData) => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } =  res.data;
+
+            // Set token lo localStorage
+            localStorage.setItem('jwtToken', token);
+
+            // Set token to Auth header
+            setAuthToken(token);
+
+            // Decode token
+            const decoded = jwt_decode(token);
+
+            // Set current user
+            dispatch(setCurrentUser(decoded));
+
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+      type: SET_CURRENT_USER,
+      payload: decoded
+  }
+};
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+
+  // Remove auth header for future requests
+    setAuthToken(false);
+
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
 };
